a03fbeb885499356a0c8534aeb1a18ef5aa538e3
EC2 Default User <ec2-user@ip-172-31-16-246.ec2.internal>
2022-01-27 10:43:04 +0000

    model controller

---
+++ db/migrate/20220127103725_create_shipping_addresses.rb
@@ -0,0 +1,12 @@
+class CreateShippingAddresses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :shipping_addresses do |t|
+      t.references :customer, foreign_key: true, null: false
+      t.string :name, null: false
+      t.string :zip_cord, null: false
+      t.text :address, null: false
+
+      t.timestamps
+    end
+  end
+end
